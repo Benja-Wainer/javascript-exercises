cd53e6c90a8f1ed6126b0901b3afc01076814121
Benjamin Wainer <123282375+Benja-Wainer@users.noreply.github.com>
2024-08-28 02:23:18 +0900

    Completes first test, repeats the string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let sumString = "";
+  for (i = 1; i <= num; i++) {
+    sumString += string;
+  }
+  return sumString;
 };
 
 // Do not edit below this line
