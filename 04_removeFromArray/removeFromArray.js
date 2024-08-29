504c6a08bdeeb9810259ad2b312d3e51c45bc562
Benjamin Wainer <123282375+Benja-Wainer@users.noreply.github.com>
2024-08-29 23:10:13 +0900

    Function passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removals) {
+  const newArray = [];
+  for (const element of array) {
+    if (removals.includes(element) === false) newArray.push(element);
+  }
+  return newArray;
 };
 
 // Do not edit below this line
