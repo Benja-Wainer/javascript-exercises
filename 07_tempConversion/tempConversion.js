691bc8fb16885f0886d62b25c7b085088daf7b8a
Benjamin Wainer <123282375+Benja-Wainer@users.noreply.github.com>
2024-08-30 17:05:46 +0900

    Both functions pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreesF) {
+  const celcius = (degreesF - 32) * (5 / 9);
+  if (Number.isInteger(celcius)) {
+    return celcius;
+  } else {
+    return parseFloat(celcius.toFixed(1), 10);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesC) {
+  const fahrenheit = degreesC * (9 / 5) + 32;
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  } else {
+    return parseFloat(fahrenheit.toFixed(1), 10);
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
